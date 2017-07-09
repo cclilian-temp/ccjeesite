ca8ec46495952fb7305b738147c36f7aeaca8e5f
cc <765256844@qq.com>
2017-07-09 16:56:30 +0800

    添加Annotation 配置

---
+++ src/test/java/ccjeesite/spring/annotation/Model.java
@@ -0,0 +1,10 @@
+package ccjeesite.spring.annotation;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Model {
+	public void test() {
+		System.out.println("Spring annotation Test Success");
+	}
+}
---
+++ src/test/java/ccjeesite/spring/annotation/SpringAnnotationTest.java
@@ -0,0 +1,13 @@
+package ccjeesite.spring.annotation;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class SpringAnnotationTest {
+	
+	public static void main(String[] args) {
+		ApplicationContext ctx = new ClassPathXmlApplicationContext("classpath*:/spring-context*.xml");
+		Model bean = (Model)ctx.getBean("model");
+		bean.test();
+	}
+}
