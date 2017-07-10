7175d5a735258c2ddd39caa448a004a0a0462702
cc <765256844@qq.com>
2017-07-10 21:27:43 +0800

    Annotation AOP

--- src/test/java/ccjeesite/aop/Two.java
+++ src/test/java/ccjeesite/aop/Two.java
@@ -5,8 +5,10 @@ import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
 
+@Aspect
 @Component
 public class Two {
+	@Before("execution(** ccjeesite.aop.One.before(..))")
     public void test()
     {
         System.out.println("横切关注点");
